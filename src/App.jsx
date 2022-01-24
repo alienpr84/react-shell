92b4568dc605dc2f393f457bee390ce3b19de709
Alien Padilla Rodriguez <alienpr84@gmail.com>
2022-01-24 17:45:27 -0500

    gitignore and some content into the files were added

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactShellLogo from './public/assets/images/logo.jpg';
+
+function App() {
+	return (
+		<div className='container'>
+			<figure>
+				<img src={ReactShellLogo} alt='react shell logo' />
+			</figure>
+		</div>
+	);
+}
+
+export default App;
